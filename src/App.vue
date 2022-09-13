b325f456bbe89d8e6bd9a9d73b317a0f522eae2f
Bar <bar.cohen@appsforce.io>
2022-09-13 15:15:14 +0300

    TheToolbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
+  <the-toolbar />
   <router-view />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import TheToolbar from "@components/TheToolbar.vue";
+
+export default defineComponent({
+  components: {
+    TheToolbar,
+  },
+});
+</script>
+
 <style lang="scss">
-#app {
+* {
+  box-sizing: border-box;
+}
+
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  width: 100%;
+  font-size: 16px;
+  margin: 0;
 }
 </style>
--- src/components/TheToolbar.vue
+++ src/components/TheToolbar.vue
@@ -1,9 +1,26 @@
 <template>
-  <nav></nav>
+  <nav>
+    <h2>Avatrade Task</h2>
+  </nav>
 </template>
 
 <script lang="ts">
-export default {};
+import { defineComponent } from "vue";
+export default defineComponent({});
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+nav {
+  width: 100%;
+  height: 72px;
+  background-color: rgb(0, 0, 107);
+  display: flex;
+  align-items: center;
+  padding: 0 4rem;
+
+  h2 {
+    color: white;
+    vertical-align: middle;
+  }
+}
+</style>
